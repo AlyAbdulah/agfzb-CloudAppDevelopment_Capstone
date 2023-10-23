ed0af0e2f8db69754476b5eacf588043a4c88c51
AlyAbdullah <accounts@alyabdulah.com>
2023-10-23 19:51:01 +0500

    Week 3 Part 2

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -74,7 +74,7 @@ def registration_request(request):
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
-        url = "https://5bde1960.us-south.apigw.appdomain.cloud/api/dealership"
+        url = "https://b29a033a-5254-41f1-b087-149a9e481f68-bluemix.cloudant.com/"
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
         # Concat all dealer's short name
